a9b616538c920c91207382ec16bcbbafbc52c624
Emily Curtin <emily@framebit.org>
2014-11-07 15:34:14 -0500

    added a standard eclipse .gitignore

---
+++ ErrorCheck.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+
+/**
+ * @author Emily
+ *
+ */
+public class ErrorCheck {
+
+}
---
+++ helloworld.java
@@ -0,0 +1,7 @@
+//helloworld.java
+
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("hey look i got stuff to build in eclipse");
+    }
+}
